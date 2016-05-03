3c2bceebf148327b4f2359409313e6f04cdb405a
Sam Liu <sam.liu@mail.searching-service.com>
2016-05-03 15:06:55 +0800

    part3-added cart

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,6 +2,7 @@ $(function () {
 
   $("#search").keyup(function () {
     var search_term = $(this).val();
+    //noinspection JSAnnotator
     $.ajax({
       method: "post",
       url: "/api/search",
@@ -18,14 +19,14 @@ $(function () {
         var html = "";
         for (var i = 0; i < data.length; i++) {
           html =
-            '<div class="col-md-4">\n  ' +
-            '<a href="/product/' + data[i]._id + '">\n    ' +
-            '<div class="thumbnail">\n      ' +
-            '<img src="' + data[i]._source.image + '">\n      ' +
-            '<div class="caption">\n        ' +
-            '<h3>' + data[i]._source.name + '</h3>\n        ' +
-            '<p>' + data[i]._source.category.name + '</p>\n        ' +
-            '<p>' + data[i]._source.price + '</p>\n      ' +
+            '<div class="col-md-4">\n' +
+            '<a href="/product/' + data[i]._id + '">\n' +
+            '<div class="thumbnail">\n' +
+            '<img src="' + data[i]._source.image + '">\n' +
+            '<div class="caption">\n' +
+            '<h3>' + data[i]._source.name + '</h3>\n' +
+            '<p>' + data[i]._source.category.name + '</p>\n' +
+            '<p>' + data[i]._source.price + '</p>\n' +
             '</div>\n' +
             '</div>\n' +
             '</a>\n' +
@@ -40,4 +41,30 @@ $(function () {
       }
     })
   })
+});
+
+$(document).on('click', '#plus', function (e) {
+  e.preventDefault();
+  var priceValue = parseFloat($('#priceValue').val());
+  var quantity = parseInt($('#quantity').val());
+
+  priceValue += parseFloat($('#priceHidden').val());
+  quantity += 1;
+  $('#quantity').val(quantity);
+  $('#priceValue').val(priceValue.toFixed(2));
+  $('#total').html(quantity);
+}).on('click', '#minus',function(e){
+  e.preventDefault();
+  var priceValue = parseFloat($('#priceValue').val());
+  var quantity = parseInt($('#quantity').val());
+  if(quantity == 1){
+    priceValue= $('#priceHidden').val();
+    quantity = 1;
+  }else{
+    priceValue -= parseFloat($('#priceHidden').val());
+    quantity -= 1;
+  }
+  $('#quantity').val(quantity);
+  $('#priceValue').val(priceValue.toFixed(2));
+  $('#total').html(quantity);
 });
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 var router = require("express").Router();
 var User = require("../models/user");
+var Cart = require("../models/cart");
 var passport = require("passport");
 var passportConf = require("../config/passport");
+var async = require('async');
+
 
 router.get('/login', function (req, res) {
   if (req.user) return res.redirect('/');
@@ -31,37 +34,50 @@ router.get('/signup', function (req, res, next) {
   res.render('accounts/signup', {
     errors: req.flash('errors')
   });
-
-
 });
 
 router.post('/signup', function (req, res, next) {
-  var user = new User();
-  user.profile.name = req.body.name;
-  user.password = req.body.password;
-  user.email = req.body.email;
-  user.profile.picture = user.gravatar();
+  async.waterfall([
+    function (callback) {
+      var user = new User();
+      user.profile.name = req.body.name;
+      user.password = req.body.password;
+      user.email = req.body.email;
+      user.profile.picture = user.gravatar();
 
-  User.findOne({
-    email: req.body.email
-  }, function (err, existingUser) {
-    if (existingUser) {
-      req.flash('errors', 'Account with that email address alreay exists!');
-      console.log(req.body.email + " is already exist");
-      return res.redirect('/signup');
-    }
-    else {
-      user.save(function (err) {
+      User.findOne({
+        email: req.body.email
+      }, function (err, existingUser) {
+        if (existingUser) {
+          req.flash('errors', 'Account with that email address alreay exists!');
+          console.log(req.body.email + " is already exist");
+          return res.redirect('/signup');
+        }
+        else {
+          user.save(function (err) {
+            if (err) return next(err);
+            //  res.json('New user has been created!');
+            //return res.redirect('/ ');
+
+            // })
+            callback(null, user);
+
+          });
+        }
+      });
+    },
+    function (user) {
+      var cart = new Cart();
+      cart.owner = user._id;
+      cart.save(function (err) {
         if (err) return next(err);
-        //  res.json('New user has been created!');
-        //return res.redirect('/ ');
         req.logIn(user, function (err) {
           if (err) return next(err);
           res.redirect('/profile');
-        })
+        });
       });
-    }
-  });
+    },
+  ])
 });
 
 router.get('/logout', function (req, res, next) {
